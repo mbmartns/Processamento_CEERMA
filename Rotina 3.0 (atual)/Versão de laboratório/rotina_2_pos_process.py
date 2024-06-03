b6e36e28bae8e042d954aad8c8025bbd5151b1c4
mbmartns <mbmatinspg@gmail.com>
2024-06-03 11:16:34 -0300

    atualização da rotina de pos-processamento, o arquivo de julian days foi atualizado e foi feito uma nova pasta apenas com dados limpos, de flags 1 e 2

--- Rotina 3.0 (atual)/Versão de laboratório/rotina_2_pos_process.py
+++ Rotina 3.0 (atual)/Versão de laboratório/rotina_2_pos_process.py
@@ -2,13 +2,35 @@ from global_land_mask import globe
 import os
 import shutil
 import datetime
+import re
+import time
 
+
+
+nomes_colunas = None
+def extrair_nomes_colunas(caminho_arquivo):
+    colunas = '\t'
+    
+    with open(caminho_arquivo, 'r') as file:
+        for linha in file:
+            # Verifica se a linha começa com "# name"
+            if linha.startswith('# name'):
+                # Utiliza regex para capturar o nome da coluna antes do ":"
+                match = re.search(r'# name \d+ = ([^:]+):', linha)
+                if match:
+                    nome_coluna = match.group(1).strip()
+                    if nome_coluna == 'latitude':
+                        nome_coluna = 'lat'
+                    if nome_coluna == 'longitude':
+                        nome_coluna = 'long'
+                    colunas += f'{nome_coluna}\t'
+    
+    return colunas
 
 def calcular_julian_days(ano, mes, dia, hora, minuto, segundo):
 
     # Criar o objeto datetime
-    data_hora = datetime.datetime(ano, mes, dia, hora, minuto, segundo, tzinfo=datetime.timezone.utc)
-    
+    data_hora = datetime.datetime(ano, mes, dia, hora, minuto, segundo, tzinfo=datetime.timezone.utc)    
     # Data e hora base para Julian Day (1 de Janeiro de 4713 a.C.)
     data_base = datetime.datetime(4713, 1, 1, 12, 0, 0, 0, datetime.timezone.utc)
     
@@ -22,9 +44,10 @@ def calcular_julian_days(ano, mes, dia, hora, minuto, segundo):
 
 # Função para ler uma linha de cada arquivo de texto e escrever em um novo arquivo
 def ler_linha_de_cada_arquivo(diretorio_origem, diretorio_destino):
+
+    global nomes_colunas  # Indica que vamos usar a variável global
+
     # copia Lista todos os arquivos copiados
-    #shutil.copytree(diretorio_origem, diretorio_destino)
-    #arquivos = os.listdir(diretorio_destino)
     valores_jd = []
 
     for arquivo in os.listdir(diretorio_origem):
@@ -35,12 +58,10 @@ def ler_linha_de_cada_arquivo(diretorio_origem, diretorio_destino):
         if os.path.isfile(caminho_arquivo_origem):
             # Copia o arquivo para o diretório de destino
             shutil.copy(caminho_arquivo_origem, caminho_arquivo_destino)
-            #print(f'Arquivo "{arquivo}" copiado para "{diretorio_destino}".')
 
     arquivos = os.listdir(diretorio_destino)
 
         # Abre o arquivo de destino para escrita
-    #with open(os.path.join(diretorio_destino, 'resultado.txt'), 'w') as arquivo_destino:
     # Itera sobre todos os arquivos de texto no diretório de origem
     for arquivo in arquivos:
         if arquivo.endswith('.cnv'):  # Verifica se é um arquivo de texto
@@ -49,10 +70,12 @@ def ler_linha_de_cada_arquivo(diretorio_origem, diretorio_destino):
             linhas_restantes = []
             print(f'Tratando informações do arquivo {arquivo}')
 
+            if nomes_colunas == None:
+                nomes_colunas = extrair_nomes_colunas(caminho_arquivo)
+
 
             # Abre cada arquivo de texto
             with open(caminho_arquivo, 'r') as arquivo_origem:
-
 
 
                 # Lê cada linha do arquivo
@@ -61,9 +84,10 @@ def ler_linha_de_cada_arquivo(diretorio_origem, diretorio_destino):
                     if linha.startswith("* NMEA UTC (Time) = "):
                         # Extrair a parte da data e hora da linha
                         data_hora_str = linha.split('= ')[1].strip()
-                        
+                        #print(data_hora_str)
                         # Converter a string para um objeto datetime
                         data_hora = datetime.datetime.strptime(data_hora_str, '%b %d %Y %H:%M:%S')
+                        #print(data_hora)
                         
                         # Extrair ano, mês, dia, hora, minuto e segundo
                         ano = data_hora.year
@@ -74,7 +98,7 @@ def ler_linha_de_cada_arquivo(diretorio_origem, diretorio_destino):
                         segundo = data_hora.second
 
                         julian_days = calcular_julian_days(ano, mes, dia, hora, minuto, segundo)
-                        valores = f'{arquivo}\t\t\t\t{julian_days}'
+                        valores = f'{arquivo}\t\t\t\t\t{julian_days}\t\t\t\t\t{data_hora_str}'
 
                         valores_jd.append(valores)
 
@@ -85,7 +109,6 @@ def ler_linha_de_cada_arquivo(diretorio_origem, diretorio_destino):
                     elif encontrou_indicador:
                         linhas_restantes.append(linha)
 
-                            #arquivo_destino.write(linha)
                         # Se não encontramos o indicador, continuamos para a próxima linha
                     else:
                         continue
@@ -99,18 +122,19 @@ def ler_linha_de_cada_arquivo(diretorio_origem, diretorio_destino):
                                      '\nTest 7 => Spike Test for Temperature' +
                                      '\nTest 8 => Spike Test for Salinity\n' +
                                      "\nFlags: '1' - Good Data     '2' - Probably Good Data    '3' - Probably Bad Data     '4' - Bad Data\n\n\n")
+               
                #precisa ser injetada pra baixo!!!!
                 for linha in linhas_restantes:
                     arquivo_origem.write(linha)
     with open(os.path.join(diretorio_destino, 'Julian_Days.txt'), 'w') as arquivo_jd:
-        arquivo_jd.write('Arquivo\t\t\t\t\tJulian Day\n')
+        arquivo_jd.write('Arquivo\t\t\t\t\t\t\tJulian Day\t\t\t\t\t\tNMEA UTC (Time)\n')
         for valores in valores_jd:
             arquivo_jd.write(f'{valores}\n')
 
 
 
-def conferir_dados(diretorio_destino):
-#def encontrar_valores_maiores_que_0_005(caminho_arquivo):
+def conferir_dados(diretorio_destino, diretorio_qc):
+
     with open(os.path.join(diretorio_destino, 'resultado.txt'), 'w') as arquivo_destino:
         arquivo_destino.write('Arquivo\t\t\tT>0.005\t\tC>0.005\n')
 
@@ -126,13 +150,14 @@ def conferir_dados(diretorio_destino):
         
         if arquivo.endswith('.cnv'):  # Verifica se é um arquivo de texto
 
-            #                 cnv_tratado.write('T')
 
             with open(caminho_arquivo, 'r+') as cnv_tratado:
-                #DIZER QUANTAS LINHAS PULAR POR CONTA DOS TESTES
+
 
                 # Lê cada linha do arquivo e armazena em uma lista
                 linhas = cnv_tratado.readlines()
+                linhas_limpas = []
+
 
                 # Retorna ao início do arquivo
                 cnv_tratado.seek(0)
@@ -151,7 +176,6 @@ def conferir_dados(diretorio_destino):
 
 
                 # Percorre cada linha armazenada
-               # for linha in linhas:
 
                             # Verifica se a linha começa com "Test"
                     if linha.startswith("Test") or linha.startswith("Flags"):
@@ -171,8 +195,7 @@ def conferir_dados(diretorio_destino):
                     resultado_7 = '\t'
                     resultado_8 = '\t'
 
-
-
+                    qc_remove = False
                     
                     if len(colunas) >= 16: #and colunas[10].replace('.', '').isdigit():
 
@@ -180,6 +203,7 @@ def conferir_dados(diretorio_destino):
                         valor_cond = float(colunas[16])
                         valor_temp = abs(valor_temp)
                         valor_cond = abs(valor_cond)
+
 
                         valor_sal = float(colunas[17])
                         valor_pres = float(colunas[6])
@@ -203,8 +227,10 @@ def conferir_dados(diretorio_destino):
                         if (spike_t1 == None):
                             spike_t1 = float(colunas[8])
                             spike_s1 = float(colunas[17])
+                            resultado_7 += '2'
+                            resultado_8 += '2'
 
-                            #print(spike_v1)
+
                         else:
                             if i < (len(linhas) - 1):
                                 linha_posterior = str(linhas[i + 1])
@@ -222,46 +248,58 @@ def conferir_dados(diretorio_destino):
                                 if valor_pres < 500:
                                     if spike_test > 6:
                                         resultado_7 += '4'
-                                        print('oi')
+                                        qc_remove = True
+
                                     else:
                                         resultado_7 += '1'
 
                                     if spike_test_sal > 0.9:
                                         resultado_8 += '4'
+                                        qc_remove = True
                                     else:
                                         resultado_8 += '1'
 
                                 else:
                                     if spike_test > 2:
                                         resultado_7 += '4'
+                                        qc_remove = True
                                     else:
                                         resultado_7 += '1'
 
 
                                     if spike_test_sal > 0.3:
                                         resultado_8 += '4'
-                                        print('deu ruim sal')
+                                        qc_remove = True
 
                                     else:
                                         resultado_8 += '1'
+
+                            else:
+                                resultado_7 += '2'
+                                resultado_8 += '2'
+
 
 
                                 
                         if (valor_pres < -5):
                             resultado_6 += '4'
+                            qc_remove = True
                         elif -5 < valor_pres < -2.4:
                             resultado_6 += '3'
+                            qc_remove = True
                         elif (2 < valor_sal < 41.0) and (-2.5 <= valor_temp <= 40):
                             resultado_6 += '1'
                         
 
                         if valor_temp == valor_sal:
                             resultado_5 += '4'
+                            qc_remove = True
                         else:
                             resultado_5 += '1'
                         
                         if globe.is_land(valor_lat, valor_long):
                             resultado_4 += '4'
+                            qc_remove = True
                         else:
                             resultado_4 += '1'
 
@@ -272,12 +310,14 @@ def conferir_dados(diretorio_destino):
                             resultado_impossible += '1'
                         else:
                             resultado_impossible += '4'
+                            qc_remove = True
 
                         # Verifica se o valor é maior que 0.005 e atualiza a linha
                         if valor_temp > 0.005:
                             valores_temp_maior.append(valor_temp)
 
                             resultado_1 += '4'
+                            qc_remove = True
 
                         else:
                             resultado_1 += '1'
@@ -286,29 +326,26 @@ def conferir_dados(diretorio_destino):
                         if valor_cond > 0.005:
                             valores_cond_maior.append(valor_cond)
                             resultado_2 += '4'
+                            qc_remove = True
 
                         else:
                             resultado_2 += '1'
 
                         linha = linha.rstrip('\n') + resultado_1 + resultado_2 + resultado_impossible + resultado_4 + resultado_5 + resultado_6 + resultado_7 + resultado_8 + '\n'
 
+                        if qc_remove == False:
+                            linhas_limpas.append(linha)
+
                     # Escreve a linha modificada de volta no arquivo
                     cnv_tratado.write(linha)
 
-                # Trunca o restante do arquivo caso o novo conteúdo seja menor
-            #    cnv_tratado.truncate()
-                            
-                    # Verifica se o valor é maior que 0.005
-                  #      if valor_temp <= 0.005 and valor_cond <= 0.005:
-                        # Se não for, adiciona a linha à lista de linhas válidas
-                   #         linhas_validas.append(linha)
-            # Reescreve o arquivo apenas com as linhas válidas
-           # with open(caminho_arquivo, 'w') as cnv_tratado:
-            #    for linha in linhas_validas:
-             #       cnv_tratado.write(linha)
+            
+            criar_arquivo_qc(linhas_limpas, diretorio_qc, arquivo)
+
+
+
         qtt_temp = len(valores_temp_maior)
         qtt_cond = len(valores_cond_maior)
-        #print(spike_v3)
         
         if qtt_temp > 0 or qtt_cond > 0:
 
@@ -322,17 +359,28 @@ def conferir_dados(diretorio_destino):
                 elif qtt_cond > 0:
                     arquivo_destino.write(f'Valores de condutividade > 0.005 = {valores_cond_maior}\n')
 
-       # return valores_maiores_que_0_005
 
+
+def criar_arquivo_qc(linhas_limpas, diretorio_qc, arquivo):
+
+    global nomes_colunas  # Indica que vamos usar a variável global
+
+    caminho_arquivo = os.path.join(diretorio_qc, arquivo)
+    with open(caminho_arquivo, 'w') as novo_arquivo:
+        novo_arquivo.write(f'{nomes_colunas}\n')
+        for linha in linhas_limpas:
+            novo_arquivo.write(linha)  # Você pode escrever algum conteúdo se desejar
 
 
 
 novo_pos_processamento = input('Já possui a pasta de pós-processamento? (s/n)').lower()
-#print(novo_pos_processamento)
+
 if novo_pos_processamento == 's':
     pasta_pos_proc = input('Digite o caminho da pasta de pós-processamento: ')
     diretorio_origem = os.path.join(pasta_pos_proc, 'Dados')
-    diretorio_destino = os.path.join(pasta_pos_proc, 'Dados_Tratados')
+    diretorio_destino = os.path.join(pasta_pos_proc, 'Dados_Flags')
+    diretorio_qc = os.path.join(pasta_pos_proc, 'Quality_Control')
+
 
 elif novo_pos_processamento == 'n':
 
@@ -345,15 +393,18 @@ elif novo_pos_processamento == 'n':
     # Criar o caminho completo para a pasta
     pasta_pos_proc = os.path.join(caminho_area_trabalho, nome_pos_proc)
     diretorio_origem = os.path.join(pasta_pos_proc, 'Dados')
-    diretorio_destino = os.path.join(pasta_pos_proc, 'Dados_Tratados')
+    diretorio_destino = os.path.join(pasta_pos_proc, 'Dados_Flags')
+    diretorio_qc = os.path.join(pasta_pos_proc, 'Quality_Control')
+
     # Criar os diretórios
     os.makedirs(pasta_pos_proc, exist_ok=True)
     os.makedirs(diretorio_origem, exist_ok=True)
     os.makedirs(diretorio_destino, exist_ok=True)
+    os.makedirs(diretorio_qc, exist_ok=True)
 
 input('Por favor, confira que todos os dados que quer tratar estejam na pasta de "Dados".\nPressione Enter após conferência.')
 ler_linha_de_cada_arquivo(diretorio_origem, diretorio_destino)
-conferir_dados(diretorio_destino)
+conferir_dados(diretorio_destino, diretorio_qc)
 
 # depois que ler e copiar em um arquivo só, com todos os dados
 #faço um novo arquivo chamado de resultado, ou dados tratados...
